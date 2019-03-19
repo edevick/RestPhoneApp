4cbd879e88eda635d657b3283c9aa84027d30491
edevick <ekdevitskaya@gmail.com>
2019-03-19 20:22:59 +0200

    added security

---
+++ src/main/java/com/RestPhoneApp/RestPhoneApp/repo/UserRepository.java
@@ -0,0 +1,8 @@
+package com.RestPhoneApp.RestPhoneApp.repo;
+
+
+import com.RestPhoneApp.RestPhoneApp.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
